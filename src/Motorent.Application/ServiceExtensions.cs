2bafe51477d316b71c804e3cb1cb92a205f7256d
pedrior <pedrojdev@gmail.com>
2024-04-14 22:38:21 -0300

    add mediatr

--- src/Motorent.Application/ServiceExtensions.cs
+++ src/Motorent.Application/ServiceExtensions.cs
@@ -1,11 +1,21 @@
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Motorent.Application;
 
 public static class ServiceExtensions
 {
+    private static readonly Assembly ApplicationAssembly = typeof(ServiceCollectionExtensions).Assembly;
+    
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddMediator();
+        
         return services;
     }
+    
+    private static void AddMediator(this IServiceCollection services) => services.AddMediatR(config =>
+    {
+        config.RegisterServicesFromAssembly(ApplicationAssembly);
+    });
 }
