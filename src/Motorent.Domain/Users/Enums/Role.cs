ea406b39ce528187a4f0933ff0f9ec69cec62ae0
pedrior <pedrojdev@gmail.com>
2024-04-17 10:46:44 -0300

    use target-type new

--- src/Motorent.Domain/Users/Enums/Role.cs
+++ src/Motorent.Domain/Users/Enums/Role.cs
@@ -4,8 +4,8 @@ namespace Motorent.Domain.Users.Enums;
 
 public sealed class Role : Enumeration<Role>
 {
-    public static readonly Role Admin = new Role("admin", 1);
-    public static readonly Role Renter = new Role("renter", 2);
+    public static readonly Role Admin = new("admin", 1);
+    public static readonly Role Renter = new("renter", 2);
     
     private Role(string name, int value) : base(name, value)
     {
