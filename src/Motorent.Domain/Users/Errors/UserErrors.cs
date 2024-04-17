42239f2d87ba9eaf9cbb9bbe28f9bbc659963793
pedrior <pedrojdev@gmail.com>
2024-04-17 09:52:47 -0300

    use error messages in english

--- src/Motorent.Domain/Users/Errors/UserErrors.cs
+++ src/Motorent.Domain/Users/Errors/UserErrors.cs
@@ -3,7 +3,7 @@ namespace Motorent.Domain.Users.Errors;
 internal static class UserErrors
 {
     public static Error DuplicateEmail(string email) => Error.Conflict(
-        "O endereço de e-email já está em uso por outro usuário.",
+        "The email address is already in use.",
         code: "user.duplicate_email",
         details: new() { ["email"] = email });
 }
