59dd275aeb15bc42858a1c8835f7677a6bcb9aee
pedrior <pedrojdev@gmail.com>
2024-04-24 08:26:33 -0300

    add migration

---
+++ src/Motorent.Infrastructure/Common/Persistence/Migrations/20240424112617_Initial.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Motorent.Infrastructure.Common.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "outbox_messages",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    type = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    data = table.Column<string>(type: "character varying(8192)", maxLength: 8192, nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    next_attempt_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    processed_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    status = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    attempt = table.Column<int>(type: "integer", nullable: false),
+                    error_type = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    error_message = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    error_details = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_outbox_messages", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "refresh_tokens",
+                columns: table => new
+                {
+                    user_id = table.Column<string>(type: "character varying(26)", maxLength: 26, nullable: false),
+                    token = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    access_token_id = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: false),
+                    expires = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    used_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    revoked_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_refresh_tokens", x => new { x.user_id, x.token });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "renters",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "character varying(26)", maxLength: 26, nullable: false),
+                    user_id = table.Column<string>(type: "character varying(26)", maxLength: 26, nullable: false),
+                    cnpj = table.Column<string>(type: "character varying(18)", maxLength: 18, nullable: false),
+                    cnh_number = table.Column<string>(type: "text", nullable: false),
+                    cnh_category = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: false),
+                    cnh_exp_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    birthdate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_renters", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "character varying(26)", maxLength: 26, nullable: false),
+                    role = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    birthdate = table.Column<DateOnly>(type: "date", nullable: false),
+                    email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    password_hash = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_renter_cnh_number",
+                table: "renters",
+                column: "cnh_number",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_renters_cnpj",
+                table: "renters",
+                column: "cnpj",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_renters_user_id",
+                table: "renters",
+                column: "user_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_email",
+                table: "users",
+                column: "email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "outbox_messages");
+
+            migrationBuilder.DropTable(
+                name: "refresh_tokens");
+
+            migrationBuilder.DropTable(
+                name: "renters");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
