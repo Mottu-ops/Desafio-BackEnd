8e18b09c3de1ebeacda2cc2ade092c9b02de9a49
Cleber Trindade <cleber.trindade.net@gmail.com>
2024-05-20 19:10:58 -0300

    Ajustando a DeliveryController

--- MotorcycleRental/src/02 - DeliveryManagementService/MotorcycleRental.DeliveryManagementService.Api/Controllers/DeliverymanController.cs
+++ MotorcycleRental/src/02 - DeliveryManagementService/MotorcycleRental.DeliveryManagementService.Api/Controllers/DeliverymanController.cs
@@ -1,6 +1,8 @@
+using FluentValidation;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MotorcycleRental.Api.Core.Identity;
+using MotorcycleRental.DeliveryManagementService.Api.Config.Extensions;
 using MotorcycleRental.DeliveryManagementService.Service.DTOs;
 using MotorcycleRental.DeliveryManagementService.Service.Services.DeliverymanService;
 
@@ -46,6 +48,56 @@ namespace MotorcycleRental.DeliveryManagementService.Api.Controllers
             List<DeliverymanFullDto> output = await _deliverymanService.GetAllAsync();
 
             return CustomResponse(output);
+        }
+
+        [ClaimsAuthorize("Delivery", "user")]
+        [HttpPost("add-deliveryman-async")]
+        public async Task<ActionResult> AddPhotoCnh([FromBody] DeliverymanAddPhotoDto deliveryman)
+        {
+            if (string.IsNullOrEmpty(deliveryman.Id.ToString()))
+            {
+                AddError("The deliverymanId parameter must be informed!");
+                return CustomResponse();
+            }
+
+            deliveryman.SetUrlImage(GetUrlLocalStorage());
+            await _deliverymanService.AddPhotoCnh(deliveryman);
+            return CustomResponse();
+        }
+
+        [ClaimsAuthorize("Delivery", "user")]
+        [HttpPut("update-deliveryman-async")]
+        public async Task<ActionResult> UpdateDeliverymanAsync(
+            [FromBody] DeliverymanFullDto deliveryman,
+            IValidator<DeliverymanFullDto> validator)
+        {
+            var validation = await validator.ValidateAsync(deliveryman);
+            if (!validation.IsValid)
+                return BadRequest(validation.Errors.ToCustomErrorResponse());
+
+            await _deliverymanService.UpdateAsync(deliveryman);
+
+            return CustomResponse();
+        }
+
+        [ClaimsAuthorize("Admin", "admin")]
+        [HttpDelete("delete-deliveryman-async")]
+        public async Task<ActionResult> DeleteDeliverymanAsync(Guid id)
+        {
+            if (string.IsNullOrEmpty(id.ToString()))
+            {
+                AddError("The id parameter must be informed!");
+                return CustomResponse();
+            }
+
+            await _deliverymanService.DeleteAsync(id);
+
+            return CustomResponse();
+        }
+
+        private string GetUrlLocalStorage()
+        {
+            return Path.Combine(_webHostEnvironment.ContentRootPath, $"App_Data\\Images\\");
         }
     }
 }
